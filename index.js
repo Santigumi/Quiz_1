c1dd97e70d23db2c8fbc66419ee5c54e5ed814ec
Santiago Gutierrez Millan <1115064912@u.icesi.edu.co>
2024-02-09 17:49:44 -0500

    Quiz 1

---
+++ index.js
@@ -0,0 +1,20 @@
+const reports = [95,85,75,65,55];
+
+function calculate(array){
+  const a = 90;
+  const b = 80;
+  const c = 70;
+  const d = 60;
+  const f = 60;
+
+  array.forEach(element => {
+    a < element;
+    console.log("A");
+  }
+  b < element;
+    console.log("B")
+  );
+
+}
+
+console.log(calculate(reports));
